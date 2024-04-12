083ecac883a9c79ecc22790c94581ba24d80a3fe
Sam Mead <samjamead@gmail.com>
2024-04-12 09:57:57 +0100

    Getting metadata from mdx pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
-import { dateStringToBinary } from '@/_utils/helpers';
-import posts from '@/_data/posts.json';
-import PostList from '@/_components/post-list';
-import { PostWithBinaryDate } from '@/_utils/types';
+import { fetchPosts } from '@/_utils/fetchPosts';
 
 export default async function Home() {
-  const parsedPosts = posts.map((post) => {
-    const binaryDate = dateStringToBinary(post.date);
-    return {
-      ...post,
-      binaryDate,
-    };
-  });
+  const posts = await fetchPosts();
 
   return (
     <div className='w-full'>
       <div className='max-w-6xl mx-auto pt-12 flex flex-col gap-8'>
-        {parsedPosts && (
-          <PostList posts={parsedPosts as PostWithBinaryDate[]} />
+        {posts && (
+          <ul>
+            {posts.map((post) => {
+              return <li key={post.slug}>{post.title}</li>;
+            })}
+          </ul>
         )}
       </div>
     </div>
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,17 +1,24 @@
-import Link from 'next/link';
+import { dateStringToBinary } from '@/_utils/helpers';
+import posts from '@/_data/posts.json';
+import PostList from '@/_components/post-list';
+import { PostWithBinaryDate } from '@/_utils/types';
 
-export const frontmatter = {
-  name: 'Posts',
-};
+export default async function Home() {
+  const parsedPosts = posts.map((post) => {
+    const binaryDate = dateStringToBinary(post.date);
+    return {
+      ...post,
+      binaryDate,
+    };
+  });
 
-export default function Page() {
   return (
-    <p>
-      Nothing to see here.{' '}
-      <Link href='/' className='underline'>
-        Go here
-      </Link>{' '}
-      instead.
-    </p>
+    <div className='w-full'>
+      <div className='max-w-6xl mx-auto pt-12 flex flex-col gap-8'>
+        {parsedPosts && (
+          <PostList posts={parsedPosts as PostWithBinaryDate[]} />
+        )}
+      </div>
+    </div>
   );
 }
