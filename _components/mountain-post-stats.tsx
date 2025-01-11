ae70d64e031f73a0f87152e869c947bfa1a2a808
Sam Mead <samjamead@gmail.com>
2025-01-11 12:18:17 +0000

    Update mountain post stats component

--- _components/mountain-post-stats.tsx
+++ _components/mountain-post-stats.tsx
@@ -1,19 +1,42 @@
 import { ArrowRightCircle, ArrowUpCircle, Clock12 } from 'lucide-react';
 
-export default function MountainPostStats({ metadata }: { metadata: any }) {
+interface MountainPostMetadata {
+  distance?: number;
+  elevation?: number;
+  duration?: number;
+}
+
+export function formatDuration(minutes: number): string {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours}hrs ${remainingMinutes}mins`;
+}
+
+export default function MountainPostStats({
+  metadata,
+}: {
+  metadata: MountainPostMetadata;
+}) {
   return (
     <div className='mb-8 flex items-center gap-8'>
-      <p className='flex items-center gap-2'>
-        <ArrowRightCircle size={24} strokeWidth={1.5} />{' '}
-        <span>{metadata.distance}km</span>
-      </p>
-      <p className='flex items-center gap-2'>
-        <ArrowUpCircle size={24} strokeWidth={1.5} />{' '}
-        <span>{metadata.distance}m</span>
-      </p>
-      <p className='flex items-center gap-2'>
-        <Clock12 size={24} strokeWidth={1.5} /> <span>6hrs 55mins</span>
-      </p>
+      {metadata.distance && (
+        <p className='flex items-center gap-2'>
+          <ArrowRightCircle size={24} strokeWidth={1.5} />{' '}
+          <span>{metadata.distance}km</span>
+        </p>
+      )}
+      {metadata.elevation && (
+        <p className='flex items-center gap-2'>
+          <ArrowUpCircle size={24} strokeWidth={1.5} />{' '}
+          <span>{metadata.elevation}m</span>
+        </p>
+      )}
+      {metadata.duration && (
+        <p className='flex items-center gap-2'>
+          <Clock12 size={24} strokeWidth={1.5} />{' '}
+          <span>{formatDuration(metadata.duration)}</span>
+        </p>
+      )}
     </div>
   );
 }
